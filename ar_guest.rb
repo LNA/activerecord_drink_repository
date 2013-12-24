4186d94fd19a13b019f64a687aa58a144ca2967b
LNA <ThisIsLaToyaNicole@gmail.com>
2013-12-23 22:04:33 -0600

    Removed lib files used for repo version

--- ar_guest.rb
+++ ar_guest.rb
@@ -4,8 +4,7 @@ module AR
 		# has_many :drinks, through: :drinks_guests, class_name: "DrinksGuests"
 		def drinks			
 			AR::DrinksGuests.where("guest_id = ?", self.id).map do |relationship|
-				relationship.drink
-			end
+				relationship.drink			end
 			
 			# drinks_array = []
 			# AR::DrinksGuests.all.each do |relationship|
